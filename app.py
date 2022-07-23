7484ac79edc364d8a18d5d4a852955eec86d2593
Antonioj562 <antoniojloyola@gmail.com>
2022-07-23 13:42:52 -0700

    Implemented Contact Form - Changed BackGround

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, flash
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'sparten.loyola@gmail.com'
-app.config['MAIL_PASSWORD'] = os.envirson.get('Gmail')
+app.config['MAIL_PASSWORD'] = os.getenv('mailP')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
@@ -19,13 +22,20 @@ def home():
 def projects():
     return render_template("projectTab.html")
 
-@app.route("/about/", methods=["POST, ", "GET"])
+@app.route("/about/", methods=["POST", "GET"])
 def about():
     if request.method == "POST":
-        flash("Email has been sent!", "info")
+        senderSub = request.form.get("subject")
+        senderEmail = request.form.get("email")
+        senderMes = request.form.get("message")
+
+        msg = Message("Portfolio Message: "+str(senderSub), 
+                        sender="no-reply@domain.com", 
+                        recipients=['antoniojloyola@gmail.com'])
+        msg.body = "Email: " + str(senderEmail) +' --- '+senderMes
+        mail.send(msg)
         return render_template("index.html")
-    else:
-        return render_template("aboutTab.html")
+    return render_template("aboutTab.html")
     
     # Check for Captcha when submit button is pressed
 
